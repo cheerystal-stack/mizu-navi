6ff7f7a55b9a51fd2e922aaef87c12c85f5fc171
cheerystal-stack <cheerystal@gmail.com>
2026-09-21 01:54:56 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='aimizu-v3';
+const CACHE='mizunavi-stable-v-next';
 const ASSETS=[
   './','./index.html','./manifest.json',
   './assets/icon-192.png','./assets/icon-512.png','./mascot.PNG',
